62c3dbb92f3ff4a99fa85339def232ff2eb3ec7f
rkmjld2 <rkmjld2@gmail.com>
2026-04-13 12:08:43 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$port = 4000;
+$user = "ax6KHc1BNkyuaor.root";
+$pass = "EP8isIWoEOQk7DSr";
+$dbname = "sensor";
+
+$conn = mysqli_init();
+
+// SSL connection
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+if (!mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, "utf8mb4");
+
+?>
