649eaf0e97592011dc46eb4e9dfd698c3d0d1c10
rkmjld2 <rkmjld2@gmail.com>
2026-04-13 12:34:52 +0530

    Update save.php

--- save.php
+++ save.php
@@ -5,15 +5,12 @@ $s1 = $_GET['s1'] ?? 0;
 $s2 = $_GET['s2'] ?? 0;
 $s3 = $_GET['s3'] ?? 0;
 
-// DEBUG: show values
-echo "Received: $s1, $s2, $s3 <br>";
-
 $sql = "INSERT INTO sensor_db (sensor1, sensor2, sensor3)
         VALUES ('$s1', '$s2', '$s3')";
 
 if ($conn->query($sql) === TRUE) {
     echo "INSERT OK";
 } else {
-    echo "ERROR: " . $conn->error;
+    echo "DB ERROR: " . $conn->error;
 }
 ?>
